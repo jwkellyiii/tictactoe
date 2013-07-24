2d9afc92723057a5107646fe7ea2a9194b003d47
John Kelly <jwkellyiii@gmail.com>
2013-07-24 11:00:13 -0400

    Add tests for the board.isFull function

--- spec/models/board.js
+++ spec/models/board.js
@@ -1,6 +1,6 @@
 define(['tictactoe'], function(TicTacToe) {
 
-    return describe('A TicTacToe Game', function() {
+    return describe('A TicTacToe Game Board', function() {
         var that = this;
         var game, board;
 
@@ -53,6 +53,38 @@ define(['tictactoe'], function(TicTacToe) {
             it('defines the number of moves needed to be played to win', function() {
                 expect(board.minimum_moves).toBeDefined();
                 expect(board.minimum_moves).toEqual(5);
+            });
+        });
+
+        describe('check if the game board is full', function() {
+            var board_full;
+
+            beforeEach(function() {
+                game.start();
+
+                board_full = game.board.isFull();
+            });
+
+            it('validates a new board is not full', function() {
+                expect(board_full).toEqual(false);
+            });
+
+            it('validates if maximum moves are not played, the board is not full', function() {
+                expect(board_full).toEqual(false);
+
+                game.board.moves = Math.floor((Math.random()*8)+1)
+                board_full = game.board.isFull();
+
+                expect(board_full).toEqual(false);
+            });
+
+            it('validates after maximum moves are played, the board is full', function() {
+                expect(board_full).toEqual(false);
+
+                game.board.moves = game.board.board.length;
+                board_full = game.board.isFull();
+
+                expect(board_full).toEqual(true);
             });
         });
 
